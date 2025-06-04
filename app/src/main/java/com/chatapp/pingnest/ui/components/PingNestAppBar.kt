ace8456729649b282c1aaeb62c71da67f132b4ff
Nishchay Makkar <nishchaymakkar5@gmail.com>
2025-06-04 12:08:16 +0530

    Feat: Create PingNestAppBar composable
    This commit introduces a new composable `PingNestAppBar`.
    
    `PingNestAppBar` is a `CenterAlignedTopAppBar` that:
    - Displays a `ProfileIcon` as its navigation icon, using the provided `fullname`.
    - Allows customization of the title and actions.
    - Supports an optional `TopAppBarScrollBehavior`.
    - Includes padding for the navigation icon.

---
+++ app/src/main/java/com/chatapp/pingnest/ui/components/PingNestAppBar.kt
@@ -0,0 +1,38 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.chatapp.pingnest.ui.components
+
+import android.R
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PingNestAppBar(
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    onNavIconPressed: () -> Unit = { },
+    title: @Composable () -> Unit,
+    fullname: String,
+    actions: @Composable RowScope.() -> Unit = {},
+) {
+    CenterAlignedTopAppBar(
+        modifier = modifier,
+        actions = actions,
+        title = title,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            ProfileIcon(
+                size = 48.dp,  // Slightly larger profile icon
+                name = fullname,
+                modifier = Modifier.padding(start = 12.dp)
+            )
+        }
+    )
+}
