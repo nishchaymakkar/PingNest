65a49c08ad9fa850aa38ec9c47b8e4fd4dc43485
Nishchay Makkar <nishchaymakkar5@gmail.com>
2025-07-02 15:10:13 +0530

    Feat: Implement chat message notifications
    This commit introduces chat message notifications using `NotificationHelper` within the `StompMessagingClient`.
    
    **`StompMessagingClient.kt`:**
    - Injected `NotificationHelper` into the `StompMessagingClient`.
    - When a new chat message is received via STOMP:
        - If the Android version is R (API level 30) or higher, `notificationHelper.sendNotification()` is called to display a system notification.
        - The notification content and sender ID are passed to the `sendNotification` method.

--- app/src/main/java/com/chatapp/pingnest/data/network/StompMessagingClient.kt
+++ app/src/main/java/com/chatapp/pingnest/data/network/StompMessagingClient.kt
@@ -1,13 +1,17 @@
 package com.chatapp.pingnest.data.network
 
+import android.os.Build
 import android.util.Log
+import com.chatapp.pingnest.data.NotificationHelper
 import com.chatapp.pingnest.data.models.ChatNotification
 import com.chatapp.pingnest.data.models.dto.ChatMessageDto
 import com.chatapp.pingnest.data.models.dto.UserDto
+import com.chatapp.pingnest.ui.mappers.toChatMessageFromNotification
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 import org.hildan.krossbow.stomp.StompClient
@@ -16,7 +20,9 @@ import org.hildan.krossbow.stomp.headers.StompSendHeaders
 import org.hildan.krossbow.stomp.headers.StompSubscribeHeaders
 import org.hildan.krossbow.stomp.frame.FrameBody
 import org.hildan.krossbow.websocket.okhttp.OkHttpWebSocketClient
-class StompMessagingClient : RealtimeMessagingClient{
+class StompMessagingClient (
+    private val notificationHelper: NotificationHelper
+): RealtimeMessagingClient{
 
     private val webSocketClient = OkHttpWebSocketClient()
     private val stompClient = StompClient(webSocketClient)
@@ -47,6 +53,9 @@ class StompMessagingClient : RealtimeMessagingClient{
                 try {
                     val notification = Json.decodeFromString<ChatNotification>(message.bodyAsText)
                     incomingMessages.emit(notification)
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                        notificationHelper.sendNotification(notification.content,notification.senderId)
+                    }
                     Log.d("StompMessagingClient", "Received message: $notification")
                 } catch (e: Exception) {
                     Log.e("Stomp", "Failed to parse message", e)
